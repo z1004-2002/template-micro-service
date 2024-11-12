0fee68d7d45360179cb52c0fad4404d4b2b5d09b
z1004-2002 <zogningfranc@gmail.com>
2024-11-12 14:54:56 +0100

    first commit

---
+++ src/test/java/com/yowyob/template_micro_service/TemplateMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yowyob.template_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
